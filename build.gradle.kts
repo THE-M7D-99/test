9dc110890467db059a3428e57321d67682342d52
M7D-99 <M7D.99.2022@gmail.com>
2023-11-08 04:03:30 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "org.example"
@@ -16,4 +17,37 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            release(MavenPublication) {
+//                from components.release
+//
+//                        groupId = 'com.github.m7devoo'
+//                artifactId = 'material-compose'
+//                version = '1.0'
+//            }
+//        }
+//    }
+//}
+
+sourceSets {
+    main {
+        java.srcDirs("src/main/java")
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["java"])
+                groupId = "com.github.THE-M7D-99"
+                artifactId = "test"
+                version = "1.0"
+            }
+        }
+    }
 }
